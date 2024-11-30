ac1d44dff1bfd52c93dbf9e49e24af8100d0d396
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-30 23:01:24 +0200

    create interfaces for code review service and github service

---
+++ src/code_guru/interfaces.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod, ABC
+
+from httpx import AsyncClient
+
+from code_guru.schemas import CodeReviewRequest, CodeReviewResponse
+
+
+class CodeReviewServiceInterface(ABC):
+    @abstractmethod
+    async def review(
+        self, code_review_request: CodeReviewRequest
+    ) -> CodeReviewResponse:
+        pass
+
+
+class GitHubServiceInterface(ABC):
+    @abstractmethod
+    def get_api_url_from_usual_url(self, usual_url: str) -> str:
+        pass
+
+    @abstractmethod
+    async def get_files_info(
+        self, client: AsyncClient,
+        url: str
+    ) -> dict[str, str]:
+        pass
